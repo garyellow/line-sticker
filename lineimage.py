3bd7d7576046fcc638a76eb54787c308da93356c
黃冠穎 <gary20011110@gmail.com>
2021-12-14 19:14:37 +0800

    update animate file

--- lineimage.py
+++ lineimage.py
@@ -1,18 +1,26 @@
-import requests
-import os
 import json
-from bs4 import BeautifulSoup
+import os
+import bs4
+import requests
+import apnggif
 
 number = input('輸入貼圖編號：')
 
+# 設定爬取貼圖網址
 url = 'https://store.line.me/stickershop/product/' + number
 html = requests.get(url)
-soup = BeautifulSoup(html.text, 'html.parser')
+soup = bs4.BeautifulSoup(html.text, 'html.parser')
 
 # 圖片目錄
-images_dir = "image/"
-if not os.path.exists(images_dir):
-    os.mkdir(images_dir)
+image_dir = 'image/'
+png_dir = 'image/png/'
+gif_dir = 'image/gif/'
+
+# 建資料夾
+if not os.path.exists(image_dir):
+    os.mkdir(image_dir)
+if not os.path.exists(png_dir):
+    os.mkdir(png_dir)
 
 # 下載貼圖
 Data = soup.find_all('li', {'class': 'mdCMN09Li FnStickerPreviewItem'})
@@ -20,11 +28,25 @@ for data in Data:
     # 將字串資料轉換為字典
     img_info = json.loads(data.get('data-preview'))
     ID = img_info['id']
-    img_file = requests.get(img_info['staticUrl'])
+
+    # 如果有動圖就從動圖檔(apng[檔名一樣是.png])
+    if img_info['animationUrl']:
+        img_file = requests.get(img_info['animationUrl'])
+        animate = True
+        if not os.path.exists(gif_dir):
+            os.mkdir(gif_dir)
+    else:
+        img_file = requests.get(img_info['staticUrl'])
+        animate = False
 
     # 儲存的路徑和主檔名
-    full_path = os.path.join(images_dir, ID)
+    png_path = os.path.join(png_dir, ID)
 
     # 儲存圖片           
-    with open(full_path + '.png', 'wb') as f:
+    with open(png_path + '.png', 'wb') as f:
         f.write(img_file.content)
+
+    # 將apng轉換成gif
+    if animate:
+        gif_path = os.path.join(gif_dir, ID)
+        apnggif.apnggif(png_path + '.png', gif_path + '.gif')
